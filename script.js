5d795c02bd320f0cbadb5e90c8006157a8c15624
dmstrong <dmstrong07@gmail.com>
2019-12-21 12:56:07 -0600

    Added some animation via Animated.css

---
+++ script.js
@@ -0,0 +1,48 @@
+const devthadude = document.querySelector('.devthadude')
+devthadude.textContent = `Hi, I'm Derrick!`;
+devthadude.classList.add('animated', 'hinge')
+devthadude.style.animationDuration = '2s'
+devthadude.style.animationDelay = '4s'
+
+const handleAnimationEnd = () => {
+    const header = document.querySelector('#header');
+    const container = document.querySelector(".container")
+    container.innerHTML = `
+    <div>
+        <h4>Below are a few of my starter projects:</h4>
+        <ul>
+            <li>
+            <a href="https://dmstrong.github.io/simplelist/" target="_blank">Simple List/Todo app feat. localStorage</a
+            >
+            </li>
+            <li>
+            <a href="https://dmstrong.github.io/todoapp/" target="_blank">React Todo app feat. React Context and Hooks</a>
+            </li>
+        </ul>
+      </div>`
+
+    container.classList.add('animate', 'jackInTheBox')
+    container.style.animationDuration = '2s';
+
+    // I ❤️ Coding
+    // const beatingheart = document.createElement('p')
+    // beatingheart.innerText = 'I ❤️ Coding';
+    // beatingheart.classList.add('animated', 'heartBeat');
+    // beatingheart.style.fontSize = '1.5em';
+    // beatingheart.style.animationDuration = '3s';
+    // beatingheart.style.animationIterationCount = 'infinite';
+
+
+    const newHeader = document.createElement('h5')
+    newHeader.innerHTML = `Check back soon for more examples. <br /><br /> Happy Coding! - Dev Tha Dude`;
+    newHeader.classList.add('animated', 'fadeInUpBig')
+    newHeader.style.animationDuration = '2s';
+    newHeader.style.animationDelay = '7s';
+    
+    // Append and Replace
+    container.appendChild(newHeader)
+    header.removeChild(header.childNodes[1])
+    container.appendChild(beatingheart)
+}
+
+devthadude.addEventListener('animationend', handleAnimationEnd)
